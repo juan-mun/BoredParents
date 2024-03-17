776f01793f44acde6e5476110b6c1d54f30ecaff
munozmann <92196342+munozmann@users.noreply.github.com>
2024-03-17 16:25:13 -0500

    Archivo para creación de ramas

---
+++ master.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprime el mensaje "¡Hola, mundo!" en la consola
+        System.out.println("¡Hola, mundo!");
+    }
+}
