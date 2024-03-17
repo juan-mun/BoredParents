c8a64ba83ab9d51dac639a7264d01a9877921ea8
munozmann <92196342+munozmann@users.noreply.github.com>
2024-03-17 16:26:55 -0500

    Archivo para creación desarrollo

--- master.java
+++ master.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         // Imprime el mensaje "¡Hola, mundo!" en la consola
         System.out.println("¡Hola, mundo!");
+	System.out.prinln("Hola qué hace");
     }
 }
