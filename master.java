0c1dd26c909186d405bba565058cfa15b6e4d233
juan-mun <juan.munoz09@usa.edu.co>
2024-03-21 23:25:44 -0500

    Archivo rama moreno

--- master.java
+++
@@ -1,9 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        // Imprime el mensaje "¡Hola, mundo!" en la consola
-        System.out.println("¡Hola, mundo!");
-	System.out.prinln("Hola qué hace");
-	System.out.prinln("Hola cómo vas");
-	System.out.prinln("Hola nueva ram");
-    }
-}
