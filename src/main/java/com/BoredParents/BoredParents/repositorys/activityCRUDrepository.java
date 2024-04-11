cb4845e20841e928b9955dff3d9e0f23558ce829
Camilo <andres.moreno04@usa.edu.co>
2024-04-11 00:10:36 -0500

    Creacion de la interfaz de seleccion e interaccion de actividades

--- src/main/java/com/BoredParents/BoredParents/repositorys/activityCRUDrepository.java
+++
@@ -1,9 +0,0 @@
-package com.BoredParents.BoredParents.repositorys;
-import org.springframework.data.repository.CrudRepository;
-
-import com.BoredParents.BoredParents.Entities.Actividad;
-;
-
-public interface activityCRUDrepository  extends CrudRepository<Actividad,Long>{
-    
-}
