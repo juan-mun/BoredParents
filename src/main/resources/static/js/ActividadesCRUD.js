7253fee58311781fc6004dd79f37e52dacf1cd15
Camilo <andres.moreno04@usa.edu.co>
2024-04-16 11:01:55 -0500

    Crear interfaz y funcionalidades del CRUD actividades

---
+++ src/main/resources/static/js/ActividadesCRUD.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+    getActividades();
+});
+
+$(document).on('click', 'table tbody tr', function() {
+    var row = $(this);
+    $('#actividadId').val(row.find('td:eq(0)').text()); // ID
+    $('#nombre').val(row.find('td:eq(1)').text()); // Nombre
+    $('#descripcion').val(row.find('td:eq(2)').text()); // Descripción
+    $('#categoria').val(row.find('td:eq(3)').text()); // Categoría
+    $('#duracion').val(row.find('td:eq(4)').text()); // Duración
+    $('#createActivityModalLabel').text('Editar Actividad');
+    $('#saveActivity').text('Guardar Cambios').attr("onclick", "updateActividad()");
+    $('#createActivityModal').modal('show');
+});
+
+function addActividad(){
+    var nombre = $('#nombre').val();
+    var descripcion = $('#descripcion').val();
+    var categoria = $('#categoria').val();
+    var duracion = $('#duracion').val();
+
+    var nuevoDato = {
+        nombre: nombre,
+        descripcion: descripcion,
+        categoria: categoria,
+        duracion: duracion
+    }
+
+    $.ajax({
+        url: '/actividades/addActivity',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(nuevoDato),
+        success: function(data) {
+            // Cierra el modal
+            $('#createActivityModal').modal('hide');
+
+            // Limpia el formulario
+            $('#createActivityForm')[0].reset();
+
+            // Actualiza la tabla de actividades
+            getActividades();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+
+}
+
+function getActividades() {
+    $.ajax({
+        url: '/actividades/getActivity', // Asegúrate de que esta URL sea correcta
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var tableBody = $('table tbody');
+            tableBody.empty(); // Limpia la tabla antes de añadir los nuevos resultados
+
+            // Itera sobre cada actividad y la añade a la tabla
+            $.each(data, function(index, actividad) {
+                tableBody.append(
+                    '<tr>' +
+                    '<td>' + actividad.id_actividad + '</td>' +
+                    '<td>' + actividad.nombre + '</td>' +
+                    '<td>' + actividad.descripcion + '</td>' +
+                    '<td>' + actividad.categoria + '</td>' +
+                    '<td>' + actividad.duracion + '</td>' +
+                    '</tr>'
+                );
+            });
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+}
+
+function updateActividad() {
+    var id = $('#actividadId').val();
+    var actividadData = {
+        nombre: $('#nombre').val(),
+        descripcion: $('#descripcion').val(),
+        categoria: $('#categoria').val(),
+        duracion: $('#duracion').val()
+    };
+
+    $.ajax({
+        url: '/actividades/updateActivity/' + id,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(actividadData),
+        success: function(data) {
+            $('#createActivityModal').modal('hide');
+            $('#createActivityForm')[0].reset();
+            getActividades();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+}
--- src/main/resources/static/js/BibliotecaVirtual.js
+++ src/main/resources/static/js/BibliotecaVirtual.js
@@ -1,7 +1,46 @@
 $(document).ready(function() {
     cargarActividades();
 });
-  
+
+function anadirAsignacion(idActividad, nombreActividad) {
+  const postData = {
+    nino: {id_nino: 1},
+    actividad: {id_actividad: idActividad}
+  };
+
+  $.ajax({
+    url: 'asignaciones/addAsignacion',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(postData),
+    success: function(response) {
+      $(`#actividad-${idActividad}`).hide();
+      const estante = $('.bookshelf .shelf').first();
+      const libro = $(`
+        <div class="book" id="book-${idActividad}">
+            <div class="book-text">${nombreActividad}</div>
+        </div>
+      `);
+      estante.append(libro);
+
+      // Actualiza la clase 'only-book' para el nuevo estado del estante.
+      actualizarEstiloDelLibro(estante);
+    },
+    error: function(xhr, status, error) {
+      console.error('error creando asignacion: ', error, xhr, status);
+    }
+  });
+}
+
+function actualizarEstiloDelLibro(estante) {
+  const libros = estante.children('.book');
+  if(libros.length === 1) {
+      $(libros[0]).addClass('only-book');
+  } else {
+      libros.removeClass('only-book');
+  }
+}
+
 function cargarActividades() {
     $.ajax({
       url: 'actividades/getActivity',
@@ -12,11 +51,11 @@ function cargarActividades() {
         listaActividadesContainer.empty(); 
         actividades.forEach(function(actividad) {
           const actividadElement = $(`
-            <div class="actividad">
+            <div class="actividad" id="actividad-${actividad.id_actividad}">
               <h5 class="titulo-actividad">${actividad.nombre}</h5>
               <h6 class="categoria-actividad">${actividad.categoria}</h6>
               <p class="duracion-actividad">Duración: ${actividad.duracion}</p>
-              <button type="button" class="btn btn-primary float-right">Añadir</button>
+              <button type="button" class="btn btn-primary float-right" onclick="anadirAsignacion(${actividad.id_actividad}, '${actividad.nombre}')">Añadir</button>
             </div>
           `);
           listaActividadesContainer.append(actividadElement);
