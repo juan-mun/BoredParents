d08e6f8afbd407847242235ed18e7a810ea3b993
Laboratorio Audiovisuales <lab@academia.loc>
2024-04-18 22:27:24 +0200

    Borre archivos innecesarios

--- src/main/java/com/BoredParents/BoredParents/Entities/Actividad.java
+++ src/main/java/com/BoredParents/BoredParents/Entities/Actividad.java
@@ -1,7 +1,11 @@
 package com.BoredParents.BoredParents.Entities;
 
-import jakarta.persistence.*;
-import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Actividad {
--- src/main/java/com/BoredParents/BoredParents/Service/CrianzaService.java
+++ src/main/java/com/BoredParents/BoredParents/Service/CrianzaService.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Service
 public class CrianzaService {
 
+
+    @Autowired
     private final resourcesRepository resourcesRepository;
 
     @Autowired
@@ -18,6 +20,6 @@ public class CrianzaService {
     }
 
     public List<Crianza> getAllResources() {
-        return resourcesRepository.findAll();
+        return resourcesRepository.getAllResources();
     }
 }
